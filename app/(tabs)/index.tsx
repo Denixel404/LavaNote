b3434fa76ccb3829ce194e165cef7493d06c9262
Denixel <anohindenis707@gmail.com>
2026-05-28 12:48:38 +0300

    Добавлен список заметок

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,82 @@
-import { Text, View } from "react-native";
+/* eslint-disable import/no-unresolved */
+import { FlatList, Text, View } from "react-native";
 import { StyleSheet } from "react-native";
-import { Link, useRouter } from "expo-router";
+import { Link } from "expo-router";
+import React from "react";
+import { useState, useCallback } from "react";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import Button from "../components/Button"
+import { getData, getFileInfo } from "@/src/scripts/fileSystem"
+import { getDisplayDate } from "@/src/scripts/utils"
 
-const router = useRouter();
+// npx expo start
 
-export default function Index() { // Основное наполнение страницы
+export default function Index() { // <- убран async
+  const nav = useNavigation();
+  const [files, setFiles] = useState<File[]>([]);
+
+  getFileInfo("Заголовок.txt");
+
+  useFocusEffect(
+    useCallback(() => {
+      const loadFiles = async () => {
+        const loadedFiles = await getData();
+        setFiles(loadedFiles);
+      };
+      loadFiles();
+    }, [])
+  )
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Здесь пока пусто. Начните работать!</Text>
-      <Link href="/settings" style={styles.sett_btn}>
-        Перейти к настройкам
-      </Link>
+      <FlatList
+        data={files}
+        keyExtractor={(item) => item.uri} 
+        renderItem={({ item }) => (
+          <View style={styles.note}>
+            <Text style={styles.note_text}>{item.name}</Text>
+            <Text style={styles.note_text_info}>{getDisplayDate(item.creationTime)}</Text>
+          </View>
+        )}
+        ListEmptyComponent={<Text style={styles.empty}>Здесь пока пусто. Начните работать!</Text>}
+      />
       <View style={styles.footerContainer}>
-        <Button label="Добавить заметку" backgroundColor="#e05807" onPress={() => {router.navigate("./newNote")}}/>
-        {/* <Button label="Открыть заметку" backgroundColor="#e05807"/> */}
+        <Button label="Добавить заметку" backgroundColor="#e05807" onPress={() => { nav.navigate("newNote") }} />
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({ // Таблица стилей
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#040332",
     alignItems: "center",
     justifyContent: "center",
   },
+  note: {
+    borderWidth: 2,
+    borderColor: "#ed8143",
+    borderRadius: 5,
+    padding: 20,
+    width: 300,
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  note_text: {
+    color: "white",
+    width: "100%"
+  },
+  note_text_info: {
+    color: "#a8a8a8",
+    width: "100%"
+  },
   text: {
     color: "#fff",
+  },
+  empty: {
+    color: "white",
   },
   sett_btn: {
     fontSize: 20,
@@ -41,5 +88,4 @@ const styles = StyleSheet.create({ // Таблица стилей
     alignItems: "center",
     margin: 50
   },
-
 })
---
+++ src/scripts/utils.tsx
@@ -0,0 +1,11 @@
+
+export const getDisplayDate = (timestamp: number) => {
+  if (!timestamp) return 'Дата неизвестна';
+  // Определяем систему: если число больше 10^11 (100 миллиардов), значит это миллисекунды
+  const isMilliseconds = timestamp > 100000000000;
+  // Создаем объект Date, корректно обрабатывая миллисекунды или секунды
+  const date = new Date(isMilliseconds ? timestamp : timestamp * 1000);
+  // Проверяем, что дата валидна
+  if (isNaN(date.getTime())) return 'Некорректная дата';
+  return date.toLocaleString('ru-RU');
+};
